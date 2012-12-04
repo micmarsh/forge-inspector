b1c1e2cb5f6a570a50e77f1fc53d39765a64c1e8
Michael Marsh <michael@michael-Aspire-5755.(none)>
2012-12-04 12:42:37 -0500

    Created addBatch function

--- src/io/trigger/forge/android/modules/database/API.java
+++ src/io/trigger/forge/android/modules/database/API.java
@@ -1,6 +1,7 @@
 package io.trigger.forge.android.modules.database;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.util.Log;
@@ -22,28 +23,50 @@ public class API {
 		}
 	}
 	
-	public static void put(final ForgeTask task, @ForgeParam("model") final JSONObject model,
-			@ForgeParam("entities") final JSONObject entities){
+	public static void addBatch(final ForgeTask task, @ForgeParam("models") final JSONArray models,
+			@ForgeParam("entities") final JSONArray entities){
 		initDB();
 		try{
-			System.out.println("**********yay model!!!!!!!!!!!!!! "+model.toString());
+			int mLength = models.length(),
+				eLength = entities.length();
+			if(mLength != eLength) throw new Exception("Array lengths not equal");
+			for(int i = 0; i < mLength;i++){
+				addWithoutInit(task, models.getJSONObject(i), models.getJSONObject(i));
+			}
 			
-			boolean update;
-			if(model.has("localID")){
-				final String id = model.getString("localID");
-				update = !id.startsWith("c");
-			}else//model is something pulled straight in from server
-				update = true;
-			
-			if(update){ 
-				notesDB.updateNoteValues(model, entities); 
-				task.success();
-			}else 
-				task.success(notesDB.addNewNote(model,entities));
 		}catch(Exception e){
 			e.printStackTrace();
 			task.error(e);
 		}
+	}
+	
+	public static void put(final ForgeTask task, @ForgeParam("model") final JSONObject model,
+			@ForgeParam("entities") final JSONObject entities){
+		initDB();
+		try{
+			addWithoutInit(task,model,entities);
+		}catch(Exception e){
+			e.printStackTrace();
+			task.error(e);
+		}
+	}
+	
+	private static void addWithoutInit(final ForgeTask task, @ForgeParam("model") final JSONObject model,
+			@ForgeParam("entities") final JSONObject entities) throws JSONException{
+		System.out.println("**********yay model!!!!!!!!!!!!!! "+model.toString());
+		
+		boolean update;
+		if(model.has("localID")){
+			final String id = model.getString("localID");
+			update = !id.startsWith("c");
+		}else//model is something pulled straight in from server
+			update = true;
+		
+		if(update){ 
+			notesDB.updateNoteValues(model, entities); 
+			task.success();
+		}else 
+			task.success(notesDB.addNewNote(model,entities));
 	}
 	
 	public static void delete(final ForgeTask task, @ForgeParam("model") final JSONObject model){
