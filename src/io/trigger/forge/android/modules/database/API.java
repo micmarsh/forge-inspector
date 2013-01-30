7f710d3a71ecd4612e2abe473e82d7371a654173
micmarsh <michael.marsh42@gmail.com>
2013-01-30 16:54:49 -0500

    removed all unneeded functions from api interface class

--- src/io/trigger/forge/android/modules/database/API.java
+++ src/io/trigger/forge/android/modules/database/API.java
@@ -23,96 +23,6 @@ public class API {
 		}
 	}
 
-	public static void put(final ForgeTask task, @ForgeParam("models") final JSONArray models,
-			@ForgeParam("entities") final JSONArray entities){
-		initDB();
-		JSONArray results = new JSONArray();//populated inside singlePut in the least FP style
-		try{
-			int mLength = models.length(),
-				eLength = entities.length();
-			if(mLength != eLength)
-				throw new Exception("Array lengths not equal");
-			for(int i = 0; i < mLength;i++)
-				singlePut(task, models.getJSONObject(i), entities.getJSONObject(i), results);
-			task.success(results);
-		}catch(Exception e){
-			e.printStackTrace();
-			task.error(e);
-		}
-	}
-
-
-	private static void singlePut(final ForgeTask task, @ForgeParam("model") final JSONObject model,
-			@ForgeParam("entities") final JSONObject entities,final JSONArray results) throws JSONException{
-		System.out.println("**********yay model!!!!!!!!!!!!!! "+model.toString());
-
-		boolean update;
-		if(model.has("localID")){
-			final String id = model.getString("localID");
-			update = !id.startsWith("c");
-		}else//model is something pulled straight in from server
-			update = true;
-
-		if(update){
-			notesDB.updateNoteValues(model, entities);
-			task.success();
-		}else{
-			String id = notesDB.addNewNote(model,entities);
-			if(results != null)
-				results.put(id);
-			else
-				task.success(id);
-		}
-	}
-
-	public static void delete(final ForgeTask task, @ForgeParam("model") final JSONObject model){
-		initDB();
-		try{
-			notesDB.deleteNote(model);
-			task.success();
-		}catch(Exception e){
-			e.printStackTrace();
-			task.error(e);
-		}
-	}
-
-	public static void getdirty(final ForgeTask task){
-		initDB();
-		try{
-			task.success(notesDB.getDirtyNotes());
-		}catch(Exception e){
-			e.printStackTrace();
-			task.error(e);
-		}
-	}
-
-	public static void getnext(final ForgeTask task, @ForgeParam("start") final int start,
-			@ForgeParam("chunkSize") final int chunkSize){
-		initDB();
-		try{
-			task.success(notesDB.getNextNotes(start, chunkSize));
-		}catch(Exception e){
-			task.error(e);
-		}
-	}
-
-	public static void getAllTags(final ForgeTask task){
-		initDB();
-		try{
-			task.success(notesDB.getTags());
-		}catch(Exception e){
-			task.error(e);
-		}
-	}
-
-	public static void fetch(final ForgeTask task, @ForgeParam("tags") JSONArray tags){
-		initDB();
-		try{
-			task.success(notesDB.fetch(tags));
-		}catch(Exception e){
-			task.error(e);
-		}
-	}
 
 	public static void entityQuery(final ForgeTask task, @ForgeParam("query") String query
 		,@ForgeParam("type") final String type){
@@ -127,7 +37,7 @@ public class API {
 
 	public static void query(final ForgeTask task, @ForgeParam("query") String query){
 		initDB();
-		try{						//runs the query, returns a JSONArray of JSONObjects
+		try{					
 			task.success(notesDB.queryToNotes(query));
 		}catch(Exception e){
 			task.error(e);
@@ -138,7 +48,8 @@ public class API {
 	public static void write(final ForgeTask task, @ForgeParam("query") String query){
 		initDB();
 		try{
-			task.success(notesDB.writeQuery(query));
+			writeAll(task,new JSONArray().put(query));
+			//task.success(notesDB.writeQuery(query));
 		}catch(Exception e){
 			e.printStackTrace();
 			task.error(e);
