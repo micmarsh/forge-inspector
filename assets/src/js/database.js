04b81e0b179e3ef10ad2b390a37afb659f40abe9
micmarsh <michael.marsh42@gmail.com>
2013-02-12 18:19:12 -0500

    Nasty "not an error" happens, commiting for the historical record

--- assets/src/js/database.js
+++ assets/src/js/database.js
@@ -5,27 +5,89 @@ Database = (function() {
 
   Database.name = 'Database';
 
+  Database.prototype.TEXT_COL = "text";
+
+  Database.prototype.LOC_COL = "localID";
+
+  Database.prototype.SERV_COL = "id";
+
+  Database.prototype.TIME_COL = "timestamp";
+
+  Database.prototype.STATUS_COL = "status";
+
   function Database() {
     this.notes.TABLE_NAMES = this.TABLE_NAMES;
     this.notes._whereClause = this._whereClause;
+    this.notes.text = this.TEXT_COL;
+    this.notes.id = this.SERV_COL;
+    this.notes.localID = this.LOC_COL;
+    this.notes.status = this.STATUS_COL;
+    this.notes.timestamp = this.TIME_COL;
   }
 
   Database.prototype.TABLE_NAMES = {
+    notes: "Notes",
     hashtags: "NoteTag",
     attags: "NoteContact",
     emails: "NoteEmail",
     urls: "NoteURL"
   };
 
+  Database.prototype.CREATE_TABLES = (function() {
+    var COLUMN, CREATE, ENTITY_COLS, LOC_ID, SCHEMA, TABLE_NAMES, i, schema, that;
+    that = Database.prototype;
+    CREATE = "CREATE TABLE ";
+    TABLE_NAMES = _.values(that.TABLE_NAMES);
+    ENTITY_COLS = ["hashtags", "attags", "emails", "urls"];
+    LOC_ID = "" + that.LOC_COL + " INTEGER PRIMARY KEY ";
+    SCHEMA = [("(" + that.TEXT_COL + " TEXT, " + LOC_ID + ", " + that.SERV_COL + " TEXT UNIQUE,") + (" " + that.TIME_COL + " TEXT, " + that.STATUS_COL + " TEXT)")].concat((function() {
+      var _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = ENTITY_COLS.length; _i < _len; _i++) {
+        COLUMN = ENTITY_COLS[_i];
+        _results.push("(" + that.LOC_COL + " INTEGER, " + COLUMN + " TEXT)");
+      }
+      return _results;
+    })());
+    return (function() {
+      var _i, _len, _results;
+      _results = [];
+      for (i = _i = 0, _len = SCHEMA.length; _i < _len; i = ++_i) {
+        schema = SCHEMA[i];
+        _results.push({
+          name: TABLE_NAMES[i],
+          schema: schema
+        });
+      }
+      return _results;
+    })();
+  })();
+
+  Database.prototype.lastSync = function(time) {
+    if (time != null) {
+      return bc.core.cache(this.last, time);
+    } else {
+      return bc.core.cache(this.last);
+    }
+  };
+
   Database.prototype._whereClause = function(args) {
     var attags, clauses, dirty, hashtags, search;
     hashtags = args.hashtags, attags = args.attags, search = args.search, dirty = args.dirty;
-    clauses = _.compact([(hashtags.length || attags.length ? " localID in (" + (Database.prototype._buildFilterQuery(hashtags, attags)) + ")" : ''), (search ? " text like '%" + search + "%' collate nocase " : ''), (dirty === true ? " sync != 'synced' " : ''), (dirty === false ? " sync != 'delete' " : '')]);
+    clauses = _.compact([(hashtags.length || attags.length ? " " + Database.prototype.LOC_COL + " in (" + (Database.prototype._buildFilterQuery(hashtags, attags)) + ")" : ''), (search ? " " + Database.prototype.TEXT_COL + " like '%" + search + "%' collate nocase " : ''), (dirty === true ? " " + Database.prototype.STATUS_COL + " != 'synced' " : ''), (dirty === false ? " " + Database.prototype.STATUS_COL + " != 'delete' " : '')]);
     if (clauses.length) {
       return "where " + clauses.join(' and ');
     } else {
       return '';
     }
+  };
+
+  Database.prototype.clear = function(options) {
+    var error, success;
+    success = options.success, error = options.error;
+    return forge.internal.call('database.dropTables', {
+      tables: _.values(this.TABLE_NAMES)
+    }, success, error);
   };
 
   Database.prototype._buildFilterQuery = function(hashtags, attags) {
@@ -44,7 +106,7 @@ Database = (function() {
         _results = [];
         for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
           item = _ref1[_j];
-          _results.push("select distinct localID from " + this.TABLE_NAMES[name] + " where " + name + " == '" + item + "'");
+          _results.push("select distinct " + Database.prototype.LOC_COL + " from " + this.TABLE_NAMES[name] + " where " + name + " == '" + item + "'");
         }
         return _results;
       }).call(this);
@@ -62,6 +124,18 @@ Database = (function() {
     });
   };
 
+  Database.prototype._getStuff = function(args) {
+    var error, newSuccess, query, success, type;
+    query = args.query, type = args.type, success = args.success, error = args.error;
+    newSuccess = function(dbData) {
+      return success(dbData, args);
+    };
+    console.log(query);
+    return forge.internal.call('database.query', {
+      query: query
+    }, newSuccess, error);
+  };
+
   Entities = (function() {
 
     Entities.name = 'Entities';
@@ -73,7 +147,7 @@ Database = (function() {
     }
 
     Entities.prototype._buildQuery = function(args) {
-      return ("select distinct " + args.type + " as name, count(" + args.type + ") as count") + (" from " + this.TABLE_NAMES[args.type] + " ") + Database.prototype._whereClause(args) + (" group by " + args.type);
+      return ("select distinct " + args.type + " as name, count(" + args.type + ") as count") + (" from " + this.TABLE_NAMES[args.type] + " ") + Database.prototype._whereClause(args) + (" group by " + args.type + ";");
     };
 
     Entities.prototype.get = function(args) {
@@ -90,10 +164,11 @@ Database = (function() {
       args.hashtags || (args.hashtags = []);
       args.attags || (args.attags = []);
       args.type = this._type;
-      return forge.internal.call('database.query', {
+      args.query = {
         query: this._buildQuery(args),
         args: []
-      }, args.success, args.error);
+      };
+      return Database.prototype._getStuff(args);
     };
 
     return Entities;
@@ -157,17 +232,17 @@ Database = (function() {
         var i, _i, _ref;
         if (_.isArray(model)) {
           for (i = _i = 0, _ref = model.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
-            model[i].set('localID', ids[i]);
+            model[i].set(_this.localID, ids[i]);
           }
         } else {
-          model.set('localID', ids[0]);
+          model.set(_this.localID, ids[0]);
         }
         return _this._writeAll(_this._buildAddEntitiesQueries(_this._arrayCheck(model)), {
           success: success,
           error: error
         });
       };
-      return this._makeAndCallQuery(model, options, this._buildAddNoteQuery, 'create');
+      return this._makeAndCallQuery(model, options, $.proxy(this._buildAddNoteQuery, this), 'create');
     },
     update: function(model, options) {
       var error, success,
@@ -202,13 +277,12 @@ Database = (function() {
       }
     },
     clean: function(model, options) {
+      options || (options = {});
       options.dirty = false;
       if (!_.isArray(model)) {
-        if (model.get('sync') === 'delete') {
+        if (model.get(this.status) === 'delete') {
           return this["delete"](model, options);
         } else {
-                    alert("sheeeeit" + JSON.stringify(model.toJSON(true)));
-
           options.cleaning = true;
           return this.update(model, options);
         }
@@ -217,19 +291,15 @@ Database = (function() {
         throw new Error("clean takes only a single object");
       }
     },
+    sync: function(models, options) {
+      console.log('fake sync function!');
+      if (options.success) return options.success();
+    },
     get: function(args) {
       args || (args = {});
       args.query = this._buildFetchQuery(args);
       args.type = "notes";
-      return this._getStuff(args);
-    },
-    _getStuff: function(args) {
-      var error, query, success, type;
-      query = args.query, type = args.type, success = args.success, error = args.error;
-      console.log(query);
-      return forge.internal.call('database.query', {
-        query: query
-      }, success, error);
+      return Database.prototype._getStuff(args);
     },
     _makeAndCallQuery: function(model, options, queryFunction, ifDirty) {
       var addQuery, cleaning, dirty, note, queries, _i, _len;
@@ -237,7 +307,7 @@ Database = (function() {
       cleaning = options.cleaning, dirty = options.dirty;
       queries = [];
       addQuery = function(note) {
-        note.set('sync', dirty ? ifDirty : 'synced');
+        note.set(this.status, dirty ? ifDirty : 'synced');
         return queries.push({
           query: queryFunction(note, cleaning),
           args: !dirty && ifDirty === 'delete' ? [] : [note.get('text')]
@@ -291,7 +361,7 @@ Database = (function() {
         _results = [];
         for (_i = 0, _len = models.length; _i < _len; _i++) {
           note = models[_i];
-          _results.push(this._buildNoteEntityQueries(Fetch.findEntities(note.get('text')), note.get('localID')));
+          _results.push(this._buildNoteEntityQueries(Fetch.findEntities(note.get(this.text)), note.get(this.localID)));
         }
         return _results;
       }).call(this));
@@ -306,7 +376,7 @@ Database = (function() {
         for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
           entity = _ref1[_j];
           results.push({
-            query: ("insert into " + this.TABLE_NAMES[entName] + " ") + ("(localID , " + entName + ") values (" + id + ",\"" + (entity.toLowerCase()) + "\")"),
+            query: ("insert into " + this.TABLE_NAMES[entName] + " ") + ("(" + this.localID + " , " + entName + ") values (" + id + ",\"" + (entity.toLowerCase()) + "\")"),
             args: []
           });
         }
@@ -318,7 +388,7 @@ Database = (function() {
       results = [];
       for (_i = 0, _len = models.length; _i < _len; _i++) {
         note = models[_i];
-        results.push(this._buildRemoveNoteEntityQueries(note.get('localID')));
+        results.push(this._buildRemoveNoteEntityQueries(note.get(this.localID)));
       }
       return _.flatten(results);
     },
@@ -330,29 +400,31 @@ Database = (function() {
           args: []
         };
       };
-      _ref = _.values(this.TABLE_NAMES);
+      _ref = _.values(this.TABLE_NAMES).slice(1);
       _results = [];
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         tableName = _ref[_i];
-        _results.push(makeObject("delete from " + tableName + " where localID=" + id));
+        _results.push(makeObject("delete from " + tableName + " where " + this.localID + "=" + id));
       }
       return _results;
     },
+    _buildSetNoteQuery: function(model) {
+      return "insert or replace into Notes" + ("(" + this.text + "," + this.id + "," + this.timestamp + "," + this.status + ") ") + (" values (?,\"" + (model.get(this.id)) + "\",") + ("\"" + (model.get(this.timestamp)) + "\",") + (" \"" + (model.get(this.status)) + "\")");
+    },
     _buildAddNoteQuery: function(model) {
-      return "insert into Notes (text,id,timestamp,sync) " + (" values (?,\"" + (model.get('id')) + "\",") + ("\"" + (model.get('timestamp')) + "\",") + (" \"" + (model.get('sync')) + "\")");
+      return ("insert into Notes (" + this.text + "," + this.id + ",") + ("" + this.timestamp + "," + this.status + ") ") + (" values (?,\"" + (model.get(this.id)) + "\",") + ("\"" + (model.get(this.timestamp)) + "\",") + (" \"" + (model.get(this.status)) + "\")");
     },
     _checkNewness: function(model) {
       if (model.isNew()) {
-        return " localID=\"" + (model.get('localID')) + "\"";
+        return " " + this.localID + "=\"" + (model.get(this.localID)) + "\"";
       } else {
-        return " id=\"" + (model.get('id')) + "\"";
+        return " " + this.id + "=\"" + (model.get(this.id)) + "\"";
       }
     },
     _buildUpdateNoteQuery: function(model, cleaning) {
-      return ("update Notes set text=?, id=\"" + (model.get('id')) + "\",") + ("timestamp=\"" + (model.get('timestamp')) + "\",") + ("sync=\"" + (model.get('sync')) + "\" where ") + (cleaning ? " localID=\"" + (model.get('localID')) + "\"" : this._checkNewness(model));
+      return ("update Notes set " + this.text + "=?, " + this.id + "=\"" + (model.get(this.id)) + "\",") + ("" + this.timestamp + "=\"" + (model.get(this.timestamp)) + "\",") + ("" + this.status + "=\"" + (model.get(this.status)) + "\" where ") + (cleaning ? " " + this.localID + "=\"" + (model.get(this.localID)) + "\"" : this._checkNewness(model));
     },
     _buildDeleteNoteQuery: function(model) {
-      console.log("apparently " + this._checkNewness + " doesn't exist");
       return "delete from Notes where " + this._checkNewness(model);
     },
     _buildFetchQuery: function(args) {
@@ -363,7 +435,7 @@ Database = (function() {
       args.limit || (args.limit = 25);
       args.dirty || (args.dirty = false);
       skip = args.skip, limit = args.limit;
-      return "select * from Notes " + this._whereClause(args) + " order by timestamp desc " + (" limit " + skip + "," + (skip + limit) + ";");
+      return "select * from Notes " + this._whereClause(args) + (" order by " + this.timestamp + " desc ") + (" limit " + skip + "," + (skip + limit) + ";");
     }
   };
 
