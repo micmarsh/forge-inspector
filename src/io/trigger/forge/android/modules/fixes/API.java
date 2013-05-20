e1ff5b4a9a93905d6607eea184b7271bfa4f3ffa
micmarsh <michael.marsh42@gmail.com>
2013-05-20 18:52:16 -0400

    need settings in place to run fixes in test app

--- src/io/trigger/forge/android/modules/fixes/API.java
+++ src/io/trigger/forge/android/modules/fixes/API.java
@@ -1,5 +1,7 @@
 package io.trigger.forge.android.modules.fixes;
 
+import android.app.Activity;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager.LayoutParams;
 import io.trigger.forge.android.core.ForgeApp;
@@ -8,13 +10,28 @@ import io.trigger.forge.android.core.ForgeTask;
 
 public class API {
 	
+	private static void log(String tag, Object msg) {
+		Log.i(tag, ""+ msg);
+	}
+	private static void log(Object message){
+		log("default tag", ""+ message);
+	}
+	
 	public static void lockScreenSize (final ForgeTask task){
 		try {	
-			LayoutParams params = ForgeApp.getActivity()
-								.getWindow()
-								.getAttributes();
+			Activity activity = ForgeApp.getActivity();
+			final Window window = activity.getWindow();
+			final LayoutParams params = window.getAttributes();
+			log("input mode before",params.softInputMode);
 			params.softInputMode = LayoutParams.SOFT_INPUT_ADJUST_NOTHING;
-			task.success();
+			log("input mode after",params.softInputMode);
+			activity.runOnUiThread(new Runnable(){
+				@Override
+				public void run() {
+					window.setAttributes(params);
+					task.success("Yay screen size is fixed");
+				}
+			});
 		} catch (Exception e) {
 			e.printStackTrace(System.err);
 			task.error(e);
